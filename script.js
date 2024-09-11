24e0195de1f783735f06db1be6d0b9d0dea257d6
David Mwita <130452351+dpm-tz@users.noreply.github.com>
2024-09-11 20:21:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,28 @@ function toggleMenu() {
     var navLinks = document.getElementById("nav-links");
     navLinks.classList.remove("show");
   }
+
+
+
+  // side bar script
+  
+  function showContent(subtopic) {
+      var contents = document.querySelectorAll('.subtopic-content');
+      contents.forEach(content => {
+          content.classList.remove('active');
+      });
+      document.getElementById(subtopic).classList.add('active');
+  }
+
+
+
+
+  //Google Translate Script 
+      function googleTranslateElementInit() {
+          new google.translate.TranslateElement({
+              pageLanguage: 'en',
+              includedLanguages: 'fr,sw,es,de', // Add the language codes you want
+              layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+          }, 'google_translate_element');
+      }
+  
