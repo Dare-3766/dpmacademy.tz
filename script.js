1529ac0b534902455b34f57a0a1fe57b109a7c87
David Mwita <130452351+dpm-tz@users.noreply.github.com>
2024-09-11 23:38:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,33 @@
-
-/// Script za menu icons
+// Menu toggle functionality
 function toggleMenu() {
-    var navLinks = document.getElementById("nav-links");
-    if (navLinks.classList.contains("show")) {
-      navLinks.classList.remove("show");
-    } else {
-      navLinks.classList.add("show");
-    }
+  var navLinks = document.getElementById("nav-links");
+  navLinks.classList.toggle("show");
+}
+
+function closeMenu() {
+  var navLinks = document.getElementById("nav-links");
+  navLinks.classList.remove("show");
+}
+
+// Toggle dark/light mode
+const toggleButton = document.getElementById('toggleMode');
+const body = document.body;
+
+toggleButton.addEventListener('click', function() {
+  if (body.classList.contains('light-mode')) {
+      body.classList.replace('light-mode', 'dark-mode');
+      toggleButton.innerHTML = '<i class="fas fa-moon"></i>';
+  } else {
+      body.classList.replace('dark-mode', 'light-mode');
+      toggleButton.innerHTML = '<i class="fas fa-sun"></i>';
   }
-  
-  function closeMenu() {
-    var navLinks = document.getElementById("nav-links");
-    navLinks.classList.remove("show");
-  }
+});
 
-
-
-  // side bar script
-  
-  function showContent(subtopic) {
-      var contents = document.querySelectorAll('.subtopic-content');
-      contents.forEach(content => {
-          content.classList.remove('active');
-      });
-      document.getElementById(subtopic).classList.add('active');
-  }
-
-
-
-
-  //Google Translate Script 
-      function googleTranslateElementInit() {
-          new google.translate.TranslateElement({
-              pageLanguage: 'en',
-              includedLanguages: 'fr,sw,es,de', // Add the language codes you want
-              layout: google.translate.TranslateElement.InlineLayout.SIMPLE
-          }, 'google_translate_element');
-      }
-  
+// Google Translate Script Initialization
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({
+      pageLanguage: 'en',
+      includedLanguages: 'fr,sw,es,de', // Add the language codes you want
+      layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+  }, 'google_translate_element');
+}
